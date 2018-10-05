37e5f176494132c6567918c6b870cef6815f9dcd
Yuma Kajihara <kajimars.uts1@gmail.com>
2018-10-06 08:59:27 +0900

    fix training setting

--- application/train.py
+++ application/train.py
@@ -104,6 +104,7 @@ def train(content, step_size, logger):
             logger.log("episode_reward", episode_reward, episode_count)
             
             episode_reward = 0
+            step = 0s
             
             # Plase add model save code as you like.
             #
